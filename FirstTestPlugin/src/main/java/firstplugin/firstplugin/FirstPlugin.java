980555aab961ee750430f8357005f24e58981f50
hexagonaldino <93714889+hexagonaldino@users.noreply.github.com>
2021-11-05 10:43:22 +0900

    Add files via upload

---
+++ FirstTestPlugin/src/main/java/firstplugin/firstplugin/FirstPlugin.java
@@ -0,0 +1,25 @@
+package firstplugin.firstplugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FirstPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+    @EventHandler
+    public void PlayerJoin(PlayerJoinEvent join){
+        join.setJoinMessage(ChatColor.GRAY+"[+]  "+ChatColor.AQUA+join.getPlayer().getName());}
+}
